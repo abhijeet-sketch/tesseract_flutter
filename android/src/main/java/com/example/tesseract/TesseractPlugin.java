4b4ca1b06b98390b2a9b0c74cdb997a38babdea6
abhijeet <sabhijeet410@gmail.com>
2021-04-15 18:05:14 +0530

    GET RECT

--- android/src/main/java/com/example/tesseract/TesseractPlugin.java
+++ android/src/main/java/com/example/tesseract/TesseractPlugin.java
@@ -3,6 +3,7 @@ package com.example.tesseract;
 import androidx.annotation.NonNull;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
+import com.googlecode.tesseract.android.ResultIterator;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -12,6 +13,8 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 import java.io.File;
 import android.os.Handler;
 import android.os.Looper;
+import android.graphics.Rect;
+import java.util.ArrayList;
 
 /** TesseractPlugin */
 public class TesseractPlugin implements FlutterPlugin, MethodCallHandler {
@@ -30,21 +33,39 @@ public class TesseractPlugin implements FlutterPlugin, MethodCallHandler {
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("extractText")) {
-      final int DEFAULT_PAGE_SEG_MODE = TessBaseAPI.PageSegMode.PSM_SINGLE_BLOCK;
+//      final int DEFAULT_PAGE_SEG_MODE = TessBaseAPI.PageSegMode.PSM_SINGLE_BLOCK;
       final String tessDataPath = call.argument("tessData");
       final String imagePath = call.argument("imagePath");
       String DEFAULT_LANGUAGE = "eng";
+
       if (call.argument("language") != null) {
         DEFAULT_LANGUAGE = call.argument("language");
       }
-      final String[] recognizedText = new String[1];
+
+      ArrayList<Rect> rectArrayList=new ArrayList<Rect>();
+      ArrayList<String> ocrTextElement = new ArrayList<String>();
+      final String recognizedText;
       final TessBaseAPI baseApi = new TessBaseAPI();
+      baseApi.setDebug(false);
       baseApi.init(tessDataPath, DEFAULT_LANGUAGE);
       final File tempFile = new File(imagePath);
-      baseApi.setPageSegMode(DEFAULT_PAGE_SEG_MODE);
+      baseApi.setPageSegMode(TessBaseAPI.PageSegMode.PSM_AUTO_OSD);
 
-      Thread t = new Thread(new MyRunnable(baseApi, tempFile, recognizedText, result));
-      t.start();
+      baseApi.setImage(tempFile);
+//      recognizedText = baseApi.getUTF8Text();
+
+      recognizedText = baseApi.getUTF8Text();
+      ResultIterator iterator = baseApi.getResultIterator();
+      iterator.begin();
+      do {
+        rectArrayList.add(iterator.getBoundingRect(TessBaseAPI.PageIteratorLevel.RIL_WORD));
+        ocrTextElement.add(iterator.getUTF8Text(TessBaseAPI.PageIteratorLevel.RIL_WORD));
+      } while (iterator.next(TessBaseAPI.PageIteratorLevel.RIL_WORD));
+      iterator.delete();
+//      recognizedText = baseApi.getUTF8Text();
+
+      baseApi.end();
+      result.success(recognizedText);
 
     } else {
       result.notImplemented();
@@ -54,36 +75,5 @@ public class TesseractPlugin implements FlutterPlugin, MethodCallHandler {
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     channel.setMethodCallHandler(null);
-  }
-}
-class MyRunnable implements Runnable {
-  private TessBaseAPI baseApi;
-  private File tempFile;
-  private String[] recognizedText;
-  private Result result;
-
-  public MyRunnable(TessBaseAPI baseApi, File tempFile, String[] recognizedText, Result result) {
-    this.baseApi = baseApi;
-    this.tempFile = tempFile;
-    this.recognizedText = recognizedText;
-    this.result = result;
-  }
-
-  @Override
-  public void run() {
-    this.baseApi.setImage(this.tempFile);
-    recognizedText[0] = this.baseApi.getUTF8Text();
-    this.baseApi.end();
-    this.sendSuccess(recognizedText[0]);
-  }
-
-  public void sendSuccess(String msg) {
-    final String str = msg;
-    final Result res = this.result;
-    new Handler(Looper.getMainLooper()).post(new Runnable() {@Override
-    public void run() {
-      res.success(str);
-    }
-    });
   }
 }
